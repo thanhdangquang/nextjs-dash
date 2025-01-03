e2eee1798683c9e90a09dc06d8d3a933b1daceb3
thanhdangquang <kwanthanh2101@gmail.com>
2025-01-03 16:26:54 +0700

    firstcm

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,7 +103,8 @@ async function seedRevenue() {
 
 export async function GET() {
   return Response.json({
-    message: "",
+    message:
+      "Uncomment this file and remove this line. You can delete this file when you are finished.",
   });
   try {
     await client.sql`BEGIN`;
